ea8f3c8b5cdb44e36ec5f90e66f38e1dfbd12901
NAZIH Walid <w95.nazih@gmail.com>
2019-06-16 23:28:36 +0200

    Updating JWT

--- Backend/AuthenticationService/src/main/java/fr/walid/nazih/security/JWTAuthenticationFilter.java
+++ Backend/AuthenticationService/src/main/java/fr/walid/nazih/security/JWTAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package fr.walid.nazih.security;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,11 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fr.walid.nazih.models.AppUser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 	
@@ -43,8 +48,23 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		super.successfulAuthentication(request, response, chain, authResult);
+		
+		User user = (User) authResult.getPrincipal();
+		
+		if(user == null) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		String jwtToken = Jwts.builder()
+								.setSubject(user.getUsername())
+								.setExpiration(new Date(System.currentTimeMillis() + 8_000_000L))
+								.signWith(SignatureAlgorithm.HS512, "w95.nazih@gmail.com")
+								.claim("roles", user.getAuthorities())
+								.compact();
+		
+		response.addHeader("Authorization", "Bearer " + jwtToken);
+		
 	}
 
 }
--- Backend/AuthenticationService/src/main/java/fr/walid/nazih/security/SecurityConfig.java
+++ Backend/AuthenticationService/src/main/java/fr/walid/nazih/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -19,7 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeRequests().anyRequest().permitAll()
+		.and()
+		.addFilter(new JWTAuthenticationFilter(authenticationManager()));
+		
 	}
 
 }

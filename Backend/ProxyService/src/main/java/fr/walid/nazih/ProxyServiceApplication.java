acaeafc4da03934e80c4cd25c6d6b4d60e48e952
NAZIH Walid <w95.nazih@gmail.com>
2019-06-18 00:14:22 +0200

    Adding ZUUL Proxy

---
+++ Backend/ProxyService/src/main/java/fr/walid/nazih/ProxyServiceApplication.java
@@ -0,0 +1,15 @@
+package fr.walid.nazih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+
+}
